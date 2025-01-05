88397b047d1254c8727ec27fe937eaf58d442c0a
aloksenapati45 <aloksenapati45@gmail.com>
2025-01-06 00:18:26 +0530

    ExcelFileUtil File Upadtes

--- Automation_Exercise/src/test/java/config/Base.java
+++ Automation_Exercise/src/test/java/config/Base.java
@@ -3,14 +3,11 @@ package config;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 
 public class Base
 {
     String Url = "https://automationexercise.com/login";
     public static WebDriver driver;
-
 
     public void Setup() {
         ChromeOptions options = new ChromeOptions();
