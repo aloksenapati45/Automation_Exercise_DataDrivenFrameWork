e64622ac0b1daa047a4ecea438b37aea0816cc0b
aloksenapati45 <aloksenapati45@gmail.com>
2025-01-07 12:56:48 +0530

    Login class added in test class and executing successfully.

--- Automation_Exercise/src/main/java/PageClass/RegisterUser.java
+++ Automation_Exercise/src/main/java/PageClass/RegisterUser.java
@@ -7,12 +7,15 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
 public class RegisterUser {
     WebDriver driver;
     WebDriverWait wait;
 
     public RegisterUser(WebDriver driver) {
         this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(40));
     }
 
     final By NameField = By.xpath("//input[@placeholder='Name']");
---
+++ Automation_Exercise/src/test/java/TestClass/Register.java
@@ -0,0 +1,50 @@
+package TestClass;
+
+import PageClass.RegisterUser;
+import config.Base;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Register extends Base {
+    private RegisterUser register;
+
+    @BeforeClass
+    public void BrowserInvoke(){
+        super.Setup();
+        register = new RegisterUser(driver);
+    }
+
+    @AfterClass
+    public void TearDown(){
+        driver.close();
+    }
+
+    @Test(description = "Register to the Application.")
+    public void register(){
+        register.EnterName("Alok");
+        register.EnterEmail("alok12@gmail.com");
+        register.ClickSignUpButton();
+        register.ClickTitle("Mr");
+        register.EnteringPassword("Alok@1234");
+        register.SelectDaysFromDropDown("19");
+        register.SelectMonthsFromDropDown("December");
+        register.SelectYearFromDropDown("2002");
+        register.SelectCheckBoxOfNewsLetter();
+        register.SelectCheckBoxOfOfferletter();
+        register.EnterFirstName("Alok");
+        register.EnterLastName("Senapati");
+        register.EnteringCompanyName("alokcorp.ltd.");
+        register.EnteringAddress1("Ameerpet");
+        register.EnteringAddress2("Hyderabad");
+        register.CheckCountryDropDownList("India");
+        register.EnteringStateName("Telangana");
+        register.EnteringCityName("Hyderabad");
+        register.EnteringZipCode("300058");
+        register.EnteringMobile("2352369856");
+        register.ClickingCreateAccountButton();
+        register.ClickingContinueButton();
+        register.ClickingDeleteAccountButton();
+        register.ClickingDeleteContinueButton();
+    }
+}
